891bd154cf351ef5df67dd64b5e653bdcd0cf6f5
aashpv <yungtommi@gmail.com>
2024-05-05 18:31:40 +0400

    add role and number for signup

--- pkg/db/create.go
+++ pkg/db/create.go
@@ -7,9 +7,11 @@ import (
 )
 
 func (p *postgres) Create(user models.User) (err error) {
-	_, err = p.db.Exec("INSERT INTO users (email, password) VALUES ($1, $2)",
+	_, err = p.db.Exec("INSERT INTO users (email, password, role, number) VALUES ($1, $2, $3, $4)",
 		user.Email,
 		user.Password,
+		user.Role,
+		user.Number,
 	)
 	if err != nil {
 		log.Printf("error occurred while creating user: %v", err)
