f84958b54f3b5e831a36fd347710bc5881a35615
aashpv <yungtommi@gmail.com>
2024-05-05 18:30:04 +0400

    add role and number for signup

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -2,8 +2,8 @@ package models
 
 type User struct {
 	//Id       int    `json:"id" db:"id"`
-	Email    string `json:"email" db:"email" validate:"email, required"`
-	Password string `json:"password" db:"password" validate:"required, min=8"`
-	//Role     string `json:"role" db:"role" validate:"required, eq=ADMIN|eq=USER"`
-	//Number   string `json:"number" db:"number" validate:"required,numeric"`
+	Email    string `json:"email" db:"email"`
+	Password string `json:"password" db:"password"`
+	Role     string `json:"role" db:"role"`
+	Number   string `json:"number" db:"number"`
 }
